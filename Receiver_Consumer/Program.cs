643c92465799d156257c1d87709a6740271f24ce
f.esmaeili <f.esmaeili@enb.local>
2024-02-07 15:18:57 +0330

    s

---
+++ Receiver_Consumer/Program.cs
@@ -0,0 +1,38 @@
+﻿
+using RabbitMQ.Client;
+using System.Text;
+using System.Threading.Channels;
+using System;
+using RabbitMQ.Client.Events;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var factory = new ConnectionFactory();
+        factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+        var connection = factory.CreateConnection();
+        var channel = connection.CreateModel();
+
+
+        channel.QueueDeclare("myQueue01",durable: true,exclusive: false,autoDelete: false,arguments: null);
+
+
+
+
+        var consumer = new EventingBasicConsumer(channel);
+
+        consumer.Received += (model, eventArg) =>
+                {
+                    var body = eventArg.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine("Received Message " + message);
+                };
+
+
+
+        channel.BasicConsume(queue: "myQueue01", autoAck: true, consumer: consumer);
+        Console.ReadLine();
+
+    }
+}
---
+++ Sender_Producer/Program.cs
@@ -0,0 +1,31 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var factory = new ConnectionFactory();
+
+        factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+        var connection = factory.CreateConnection();
+        var channel = connection.CreateModel();
+
+        channel.QueueDeclare("myQueue01", durable: true, exclusive: false, autoDelete: false, arguments: null);
+
+        channel.QueueDeclare("myQueue02", false, false, false, null);
+
+        for (int i = 0; i < 100; i++)
+        {
+            string message = $"Send shopping cart information to place an order  Time :{DateTime.Now.Ticks}";
+            var body = Encoding.UTF8.GetBytes(message);
+            channel.BasicPublish("", "myQueue01", null,body);
+        }
+     
+       
+        channel.Close();
+        connection.Close();
+
+    }
+}
+
