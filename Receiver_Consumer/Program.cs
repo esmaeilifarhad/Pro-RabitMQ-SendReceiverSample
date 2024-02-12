65d4e085d7871d49c8acea2a44587931b852fb27
f.esmaeili <f.esmaeili@enb.local>
2024-02-12 11:17:04 +0330

    d

--- Receiver_Consumer/Program.cs
+++ Receiver_Consumer/Program.cs
@@ -9,12 +9,15 @@ class Program
 {
     static void Main(string[] args)
     {
+        //ارتباط با rabbitMq
         var factory = new ConnectionFactory();
         factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
         var connection = factory.CreateConnection();
+        //ایجاد کانال
         var channel = connection.CreateModel();
 
-
+        //ایجاد صف
+        
         channel.QueueDeclare(
             queue:"myQueue01",
             durable: true,
@@ -26,17 +29,22 @@ class Program
 
 
         var consumer = new EventingBasicConsumer(channel);
-
+        //دریافت پیام
         consumer.Received += (model, eventArg) =>
                 {
                     var body = eventArg.Body.ToArray();
                     var message = Encoding.UTF8.GetString(body);
+
+                    Random random= new Random();
+                    int sleep = random.Next(0,2) * 1000;
+                    Console.WriteLine($"Sleep:{sleep} delivery tags{eventArg.DeliveryTag}");
+                    Thread.Sleep( sleep );
                     Console.WriteLine("Received Message " + message);
                     channel.BasicAck(eventArg.DeliveryTag, true);
                 };
 
 
-
+        //عکس العمل به فرستنده پیام
         channel.BasicConsume(
             queue: "myQueue01", 
             autoAck: false, 
